78450523eb8cd4c83f816f0a1b795d0124a71629
RH8888 <118464686+RH8888@users.noreply.github.com>
2025-11-01 21:04:24 +0330

    Revert "Expose get_app_key from bot package"

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,5 @@
-"""Bot package exposing public bot helpers."""
+"""Bot package exposing the dispatcher factory."""
 
 from .dispatcher import BotDispatcher
-from .services import get_app_key
 
-__all__ = ["BotDispatcher", "get_app_key"]
+__all__ = ["BotDispatcher"]
